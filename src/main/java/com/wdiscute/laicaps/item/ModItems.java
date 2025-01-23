66596d045c6b9a458089af07f38ab806010be50f
wdiscute <lucasalvesbispo2@gmail.com>
2025-01-23 20:52:31 +0000

    added an item lol

---
+++ src/main/java/com/wdiscute/laicaps/item/ModItems.java
@@ -0,0 +1,27 @@
+package com.wdiscute.laicaps.item;
+
+import com.wdiscute.laicaps.Laicaps;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems
+{
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Laicaps.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+
+}
